2fe20094cc45db546878c3f8245c26af382ea7ce
Jesper Høy <jesper@jhsoftware.dk>
2021-10-13 15:48:38 +0200

    for vNext

--- RegexIgnore.cs
+++ RegexIgnore.cs
@@ -1,71 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using JHSoftware.SimpleDNS;
+using JHSoftware.SimpleDNS.Plugin;
 
 namespace RegexIgnore
 {
-    public class RegexIgnore : JHSoftware.SimpleDNS.Plugin.IIgnoreRequestPlugIn
+  public class RegexIgnore : IIgnoreRequest, IOptionsUI
+  {
+    private System.Text.RegularExpressions.Regex rx;
+
+    IHost _Host;
+    IHost IPlugInBase.Host { get => _Host; set => _Host=value; }
+
+    public JHSoftware.SimpleDNS.Plugin.IPlugInBase.PlugInTypeInfo GetPlugInTypeInfo()
     {
-        public event JHSoftware.SimpleDNS.Plugin.IPlugInBase.LogLineEventHandler LogLine;
-        public event JHSoftware.SimpleDNS.Plugin.IPlugInBase.SaveConfigEventHandler SaveConfig;
-        public event JHSoftware.SimpleDNS.Plugin.IPlugInBase.AsyncErrorEventHandler AsyncError;
-
-        private System.Text.RegularExpressions.Regex rx;
-
-        public JHSoftware.SimpleDNS.Plugin.IPlugInBase.PlugInTypeInfo GetPlugInTypeInfo()
-        {
-            JHSoftware.SimpleDNS.Plugin.IPlugInBase.PlugInTypeInfo rv;
-            rv.Name = "Regex Ignore";
-            rv.Description = "Ignore DNS requests where requested name matches a regular expression";
-            rv.InfoURL = "";
-            rv.ConfigFile = false;
-            rv.MultiThreaded = false;
-            return rv;
-        }
-
-        public bool IgnoreRequest(JHSoftware.SimpleDNS.Plugin.IDNSRequest request)
-        {
-            return rx.IsMatch(request.QName.ToString());
-        }
-
-        public JHSoftware.SimpleDNS.Plugin.DNSAskAbout GetDNSAskAbout()
-        {
-            return new JHSoftware.SimpleDNS.Plugin.DNSAskAbout();
-        }
-
-        public JHSoftware.SimpleDNS.Plugin.OptionsUI GetOptionsUI(Guid instanceID, string dataPath)
-        {
-            return new SettingsUI();
-        }
-
-        public bool InstanceConflict(string config1, string config2, ref string errorMsg)
-        {
-            return false;
-        }
-
-        public void LoadConfig(string config, Guid instanceID, string dataPath, ref int maxThreads)
-        {
-            rx = new System.Text.RegularExpressions.Regex(config, System.Text.RegularExpressions.RegexOptions.Compiled);
-        }
-
-        public void LoadState(string state)
-        {
-            return;
-        }
-
-        public string SaveState()
-        {
-            return null;
-        }
-
-        public void StartService()
-        {
-            return;
-        }
-
-        public void StopService()
-        {
-            return;
-        }
+      JHSoftware.SimpleDNS.Plugin.IPlugInBase.PlugInTypeInfo rv;
+      rv.Name = "Regex Ignore";
+      rv.Description = "Ignore DNS requests where requested name matches a regular expression";
+      rv.InfoURL = "https://simpledns.plus/kb/184/regex-ignore-plug-in";
+      return rv;
     }
+
+    public System.Threading.Tasks.Task<bool> IgnoreRequest(IDNSRequest request)
+    {
+      return System.Threading.Tasks.Task.FromResult(rx.IsMatch(request.QName.ToString()));
+    }
+
+    public OptionsUI GetOptionsUI(Guid instanceID, string dataPath)
+    {
+      return new SettingsUI();
+    }
+
+    public bool InstanceConflict(string config1, string config2, ref string errorMsg)
+    {
+      return false;
+    }
+
+    public void LoadConfig(string config, Guid instanceID, string dataPath)
+    {
+      rx = new System.Text.RegularExpressions.Regex(config, System.Text.RegularExpressions.RegexOptions.Compiled);
+    }
+
+    public void LoadState(string state)
+    {
+      return;
+    }
+
+    public string SaveState()
+    {
+      return null;
+    }
+
+    public System.Threading.Tasks.Task StartService()
+    {
+      return System.Threading.Tasks.Task.CompletedTask;
+    }
+
+    public void StopService()
+    {
+      return;
+    }
+  }
 }
